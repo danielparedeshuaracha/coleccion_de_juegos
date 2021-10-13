1fd29786194bfac3353bad0713fc810fb4c32dff
DanielParedesH <daniel.paredes.h@tecsup.edu.pe>
2021-10-13 16:11:06 -0700

    primer commit

---
+++ ColeccionDeJuegos1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos1
+//
+//  Created by usuario on 10/13/21.
+//  Copyright © 2021 empresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
